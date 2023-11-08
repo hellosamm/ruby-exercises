c78c4f48187a4e31522ef7e88189d2faacbeb2fe
hellosamm <samanthageddes1@gmail.com>
2023-11-08 12:58:59 -1000

    m-"adding follow_along file"

---
+++ ruby_basics/follow_along_practice/variables.rb
@@ -0,0 +1,19 @@
+# collects user's name
+# puts "what is your name?"
+# name = gets.chomp
+# 10.times do
+#   puts "hello " + name
+# end
+
+# collects user's age
+# puts "how old are you?"
+# age = gets.chomp.to_i
+# puts "in 10 years you will be:"
+# puts age + 10
+
+# collects user's first name and last name
+# puts "what is your first name?"
+# first_name = gets.chomp
+# puts "what is your last name?"
+# last_name = gets.chomp
+# puts first_name + " " + last_name
